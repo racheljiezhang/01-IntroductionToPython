12eaaa990d9647d1ec2e1fa62f29fd0935861748
zhangrj <zhangrj@rose-hulman.edu>
2018-08-30 10:19:59 -0400

    Hello

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,15 +2,15 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
-         their colleagues and Rachel ZHang.
+         their colleagues and Rachel Zhang.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DOE: 2.
 #   Write code that accomplishes the following:
 #     - Constructs a SimpleTurtle with a  blue  Pen.
 #     - Makes the SimpleTurtle go straight UP 200 pixels.
@@ -38,3 +38,17 @@ Authors: David Mutchler, Dave Fisher, Vibha Alangar, Amanda Stouder,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ###############################################################################
+import rosegraphics as rg
+window = rg.TurtleWindow()
+
+stalin = rg.SimpleTurtle('circle')
+stalin.pen = rg.Pen('blue', 1)
+stalin.pen_up()
+stalin.left(90)
+stalin.forward(200)
+stalin.go_to(rg.Point(100, -40))
+stalin.pen_down()
+stalin.pen = rg.Pen('green', 10)
+stalin.backward(150)
+
+window.close_on_mouse_click()
